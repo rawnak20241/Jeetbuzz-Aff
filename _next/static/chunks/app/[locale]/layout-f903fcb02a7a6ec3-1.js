4f24076f5c9465a780ffbbf25841506adda4dc5e
besi-v1.1 <rahul2626khan@gmail.com>
2025-04-01 04:42:20 +0600

    first commit

---
+++ _next/static/chunks/app/[locale]/layout-f903fcb02a7a6ec3-1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[450],{1388:(e,s,n)=>{Promise.resolve().then(n.bind(n,3755)),Promise.resolve().then(n.bind(n,8193)),Promise.resolve().then(n.bind(n,8379)),Promise.resolve().then(n.t.bind(n,347,23))},347:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[690,699,441,517,358],()=>s(1388)),_N_E=e.O()}]);
